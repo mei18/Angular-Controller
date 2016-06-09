97c9708795d61eb9003123f850507ef1d319a5f4
Melanie Liang <melly.strearshy@gmail.com>
2016-06-08 19:46:11 -0600

    add and remove todos

--- controllers.js
+++ controllers.js
@@ -1,9 +1,24 @@
 angular.module('todoApp', [])
 
 .controller('todoController', function() {
-	var todo = this;
-	todo.list = [
+	var todoList = this;
+	todoList.list = [
 		{text: 'Aprender angular.', done: true},
 		{text: 'Hacer la tarea.', done: false}
 	];
+
+	todoList.add = function(){
+		todoList.list.push({text: todoList.todoText, done: false});
+		todoList.todoText='';
+	};
+
+	todoList.archive = function() {
+		var oldTodos = todoList.list;
+		todoList.list = [];
+		angular.forEach(oldTodos, function(oldTodo) {
+			if (!oldTodo.done) {
+				todoList.list.push(oldTodo);
+			};
+		});
+	};
 });
